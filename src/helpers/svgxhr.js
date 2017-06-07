9b7e40d32e14286ae9b1832db9cc44a8aecc65bd
Sylvan Mably <smably@ebay.com>
2017-06-07 16:27:44 -0400

    Use absolute URL in SVG XHR request

--- src/helpers/svgxhr.js
+++ src/helpers/svgxhr.js
@@ -6,31 +6,18 @@
  * @return {[type]}     [description]
  */
 var svgXHR = function(options) {
-  var url = false;
-  var baseUrl = undefined;
-
-  options && options.filename
-    ? url = options.filename
+  var url = options && options.filename
+    ? options.filename
     : null;
 
   if (!url) return false;
   var _ajax = new XMLHttpRequest();
-  var _fullPath;
 
   if (typeof XDomainRequest !== 'undefined') {
     _ajax = new XDomainRequest();
   }
 
-  if (typeof baseUrl === 'undefined') {
-    if (typeof window.baseUrl !== 'undefined') {
-      baseUrl = window.baseUrl;
-    } else {
-      baseUrl = window.location.protocol + '//' + window.location.hostname + (window.location.port ? ':' + window.location.port : '');
-    }
-  }
-
-  _fullPath = (baseUrl + '/' + url).replace(/([^:]\/)\/+/g, '$1');
-  _ajax.open('GET', _fullPath, true);
+  _ajax.open('GET', url, true);
   _ajax.onprogress = function() {};
   _ajax.onload = function() {
     if(!_ajax.responseText || _ajax.responseText.substr(0, 4) !== "<svg") {
